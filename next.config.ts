eca3ceba9732b35024417cef419e8a0c0fbe846d
ahmad <ahmad.94192@gmail.com>
2025-07-07 01:47:35 +0700

    'deploy'
    Signed-off-by: ahmad <ahmad.94192@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts');
+const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  // Optional: Add images config if using Next.js Image
+  images: {
+    unoptimized: true, // Required for static export
+  },
+};
 
 export default withNextIntl(nextConfig);
